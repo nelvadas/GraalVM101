b27a839f1e68f9cac0be1fd9ccae31d48e842365
nelvadas <nelvadas@gmail.com>
2022-09-23 09:55:36 +0200

    Native tests

---
+++ BondPricing/src/test/java/com/oracle/graalvm/demo/BondPricingApplicationTests.java
@@ -0,0 +1,23 @@
+package com.oracle.graalvm.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BondPricingApplicationTests {
+
+
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+
+}

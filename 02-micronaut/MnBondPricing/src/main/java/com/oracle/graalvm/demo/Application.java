f0b994d419da79d7e95fc1753dc3e1b5fc98231b
nelvadas <nelvadas@gmail.com>
2022-11-24 13:18:16 +0100

    Native image build

--- 02-micronaut/MnBondPricing/src/main/java/com/oracle/graalvm/demo/Application.java
+++ 02-micronaut/MnBondPricing/src/main/java/com/oracle/graalvm/demo/Application.java
@@ -1,9 +1,16 @@
 package com.oracle.graalvm.demo;
 
+import io.micrometer.core.instrument.Tags;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.micronaut.configuration.metrics.micrometer.prometheus.PrometheusMeterRegistryFactory;
 import io.micronaut.runtime.Micronaut;
 
+
 public class Application {
+
+
     public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+;        Micronaut.run(Application.class, args);
     }
 }

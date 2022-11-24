7657dc248d435d08b0186c39c5b508aa607d1ca9
nelvadas <nelvadas@gmail.com>
2022-11-24 18:51:08 +0100

    Verrazzano resources

---
+++ generateLoad.sh
@@ -0,0 +1,4 @@
+hey -z 10m 'https://bondpricer-jit-appconf.ms-dev.141.148.54.96.nip.io/price/graalvm/100/10/?yield=0.03&interestRate=0.02' &
+hey -z 10m 'https://bondpricer-native-ce-appconf.ms-dev-ce.141.148.54.96.nip.io/price/graalvm/100/10/?yield=0.03&interestRate=0.02' &
+hey -z 10m 'https://bondpricer-native-ee-appconf.ms-dev-ee.141.148.54.96.nip.io/price/graalvm/100/10/?yield=0.03&interestRate=0.02' &
+

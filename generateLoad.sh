a1c8ca01c6266cf9eda7faa9a4932f12fc17e3a7
nelvadas <nelvadas@gmail.com>
2024-10-24 09:42:11 +0300

    Workshop

--- generateLoad.sh
+++
@@ -1,3 +0,0 @@
-hey -z 10m 'https://bondpricer-jit-appconf.ms-dev.141.148.54.96.nip.io/price/graalvm/100/10/?yield=0.03&interestRate=0.02' &
-hey -z 10m 'https://bondpricer-native-ee-appconf.ms-dev-ee.141.148.54.96.nip.io/price/graalvm/100/10/?yield=0.03&interestRate=0.02' &
-
